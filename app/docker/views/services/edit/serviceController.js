4ffe0c650fda127c4caf34c231ca2a286753e0b3
Chaim Lev-Ari <chiptus@gmail.com>
2020-12-16 11:39:25 +0200

    feat(service-edit): show rate limit messages

--- app/docker/views/services/edit/serviceController.js
+++ app/docker/views/services/edit/serviceController.js
@@ -85,6 +85,8 @@ angular.module('portainer.docker').controller('ServiceController', [
     NetworkService,
     endpoint
   ) {
+    $scope.endpoint = endpoint;
+
     $scope.state = {
       updateInProgress: false,
       deletionInProgress: false,
@@ -525,6 +527,11 @@ angular.module('portainer.docker').controller('ServiceController', [
         },
       });
     };
+
+    $scope.setPullImageValidity = setPullImageValidity;
+    function setPullImageValidity(validity) {
+      $scope.state.pullImageValidity = validity;
+    }
 
     $scope.updateService = function updateService(service) {
       let config = {};
